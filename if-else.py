4151da3c759be950d6b056e22a3653cf3f131ec7
johngartsu <john.gartsu@gmail.com>
2022-05-28 17:44:55 -0400

    fixed range issue

--- if-else.py
+++ if-else.py
@@ -32,7 +32,7 @@ def odd_or_even_weird(n):
         print("Weird")
     if n % 2 == 0 and n in range(2,6):
         print('Not Weird')
-    if n % 2 == 0 and n in range(6,20):
+    if n % 2 == 0 and n in range(6,21):
         print("Weird")
     if n % 2 == 0 and n > 20:
         print("Not Weird")
---
+++ readme.md
@@ -0,0 +1,2 @@
+### John Gartsu Hacker Rank Code
+1. if-else.py
