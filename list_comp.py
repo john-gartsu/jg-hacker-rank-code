3f962caba4e49c04ff2ce607590c987c69888f6f
johngartsu <john.gartsu@gmail.com>
2022-06-03 16:42:55 -0400

    day-2prep

---
+++ list_comp.py
@@ -0,0 +1,10 @@
+'''
+list comprehension
+'''
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    print
---
+++ print_fn.py
@@ -0,0 +1,10 @@
+'''
+print fn problem
+https://www.hackerrank.com/challenges/python-print/problem
+'''
+
+# solution 1
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end='')
