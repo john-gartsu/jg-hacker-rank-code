c05f11a17e34838e432c73c70ca7f7b362faccca
johngartsu <john.gartsu@gmail.com>
2022-06-27 08:27:18 -0400

    10 days js

---
+++ python/1-month-prepkit/mini-maxi-sum.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+# https://www.hackerrank.com/challenges/one-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+# 
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr.sort()
+    hold = [None]*int(len(arr)-3)
+    for i in range(0,len(arr)-3):
+        temp = 0
+        for j in range(i,i+4):
+            temp = temp + arr[j]
+        hold[i] = temp
+    
+    print(hold[0],hold[-1])
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
