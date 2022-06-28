8fcaa72ba002c5b87894d9f36937182024cfe7b4
johngartsu <john.gartsu@gmail.com>
2022-06-28 08:29:06 -0400

    review required

---
+++ 30days/python/30-nested-logic.py
@@ -0,0 +1,21 @@
+# https://www.hackerrank.com/challenges/30-nested-logic/problem?isFullScreen=true
+
+# 
+# d = day, m= month, y = year
+# 
+
+def lib_fine_calc(d,m,y):
+    n = input()
+x = list(map(int, n.split()))
+m = input()
+y = list(map(int, m.split()))
+z=0
+if y[2] < x[2]:
+    z = 10000
+elif y[2] == x[2]:
+    if y[1] < x[1]:
+        z = 500*(x[1]-y[1])
+    elif y[0] < x[0]:
+        z = 15*(x[0]-y[0])
+
+print(z)
